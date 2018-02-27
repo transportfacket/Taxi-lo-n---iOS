19985ef4c49a04480fa70e9d1d9b299aaab72659
Patrik Persson <patrik.persson@transport.se>
2018-02-27 13:36:15 +0100

    Added things

--- Taxi lön/ViewControllers/ViewController.swift
+++ Taxi lön/ViewControllers/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
    
+
+    var tapCount = 0
+    var bombSoundEffect: AVAudioPlayer?
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: false)
-     
+        
+        
+        
         
     }
     
@@ -24,16 +31,62 @@ class ViewController: UIViewController {
     }
     
     
+    
+    
+    @IBAction func button(_ sender: Any) {
+   
+        
+        
+        if tapCount < 11 {
+            tapCount += 1
+        } else {
+            showAlertWith(title: "Du har hittat hit!", message: "Denna app utvecklades av Patrik Persson på Transports avdelning 12 i Malmö")
+            
+            tapCount = 0
+        }
+    }
+    
+    
+    
+    func showAlertWith(title: String, message: String, style: UIAlertControllerStyle = .alert){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: dismissAlert)
+        
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+        
+        let path = Bundle.main.path(forResource: "audio.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+            bombSoundEffect?.play()
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    func dismissAlert(sender: UIAlertAction) -> Void {
+        bombSoundEffect?.stop()
+        
+    }
+    
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
- 
+    
+    
+    
 }
 
